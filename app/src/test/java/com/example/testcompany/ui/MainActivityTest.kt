82eaec60d7579b48963726bf8b643bada3c6650c
gauravgupta566 <gaurav@umenit.com>
2020-09-21 01:26:29 +0530

    test case

--- app/src/test/java/com/example/testcompany/ui/MainActivityTest.kt
+++ app/src/test/java/com/example/testcompany/ui/MainActivityTest.kt
@@ -1,11 +1,17 @@
 package com.example.testcompany.ui
 
+import com.example.testcompany.model.Page
 import com.example.testcompany.ui.activity.MainActivity
-import com.example.testcompany.viewmodel.DataComedy
+import com.nhaarman.mockito_kotlin.any
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.modules.junit4.PowerMockRunner
 
+@RunWith(PowerMockRunner::class)
 class MainActivityTest {
 
     lateinit var mainActivity: MainActivity
@@ -16,4 +22,24 @@ class MainActivityTest {
        mainActivity=Mockito.spy(MainActivity::class.java)
 
     }
+
+    @Test
+    fun checkInputMethod(){
+      val  method = MainActivity::class.java.getDeclaredMethod(
+            "getInputManager")
+        method.isAccessible=true
+        method.invoke(mainActivity)
+        PowerMockito.doReturn(any()).`when`(mainActivity,"getInputManager")
+
+    }
+
+    @Test
+    fun addingData(){
+        val  method = MainActivity::class.java.getDeclaredMethod(
+            "addingData", any())
+        method.isAccessible=true
+        method.invoke(mainActivity)
+
+    }
+
 }
